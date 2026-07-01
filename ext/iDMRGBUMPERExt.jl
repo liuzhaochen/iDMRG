65c1361b31dca988e0c1f806b4c1d890da5f959e
ZCL_WSL_LapTop <q478051600@outlook.com>
2026-07-01 18:01:17 +0200

    Fix: correct typos in buffer part

--- ext/iDMRGBUMPERExt.jl
+++ ext/iDMRGBUMPERExt.jl
@@ -5,7 +5,7 @@ using Bumper
 using iDMRG.ITensors
 using iDMRG.ITensors.NDTensors
 import iDMRG: AbstractBuffer, with_alloc_buffer, to_buffer, reset!,
-    move_to_heap, checkpoint_buffer, buffer_restore!, buffer_size
+    move_to_heap, buffer_size, resize_buffer!, free_buffer!, lanczo_cache_buffer
 struct Allocator <: AbstractBuffer
     buf::Bumper.ResizeBuffer
     function Allocator(size=1048576)
@@ -20,26 +20,46 @@ buffer_restore!(cp::Bumper.ResizeBufferImpl.ResizeCheckpoint) = Bumper.checkpoin
 @noinline function resize!(b::Bumper.ResizeBuffer)
     Libc.free(b.buf)
     target = b.max_offset * 11 ÷ 10
-    b.buf = Libc.malloc(Int(target))
+    b.buf = Libc.malloc(target)
     b.buf_len = target
+    b.offset = UInt(0)
+    b.max_offset = UInt(0)
     return b
 end
-function resize_buffer!(bsize, A::Allocator)
-    Libc.free(A.buf.buf)
-    A.buf.buf = Libc.malloc(Int(A.buf.bsize))
-    A.buf.max_offset = UInt(0)
-    #reset buffer to initial condition
-    Bumper.reset_buffer!(buf.buf)
+function allocate_buffer!(A::Allocator)
+    #using current max_offset to allocate a new buffer
+    resize!(A.buf)
+    return A
 end
-function reset!(buf::Allocator)
-    resize!(buf.buf)
-    Bumper.reset_buffer!(buf.buf)
-    return buf
+function free_buffer!(A::Allocator)
+    Libc.free(A.buf.buf)
+    A.buf.buf = Libc.malloc(2^20)
+    A.buf.buf_len=UInt(2^20)
+    return A
+    #free buffer size but keep max_offset unchanged
+end
+function reset!(A::Allocator)
+    resize!(A.buf)
+    #this will reset overflow part of pointer
+    Bumper.reset_buffer!(A.buf)
+    return A
 end
 move_to_heap(A, buf::Allocator) = copy(A)
 
 function buffer_size(bsize, buf::Allocator)
     return buf.buf.buf_len
+end
+function lanczo_cache_buffer(L, R, psi, A::Allocator)
+    return ITensors.lanczos_permute(L, R, psi, A.buf)
+end
+function lanczo_cache_buffer(L, R, H0, H1, psi, A::Allocator)
+    # return ITensors.lanczos_permute(L, R, psi, A.buf)
+    L1 = to_buffer(L, A)
+    R1 = to_buffer(R, A)
+    H0_b = to_buffer(H0, A)
+    H1_b = to_buffer(H1, A)
+    psi = to_buffer(psi, A)
+    return L1, R1, H0_b, H1_b, psi
 end
 
 end
--- src/allocator/buffer.jl
+++ src/allocator/buffer.jl
@@ -1,3 +1,18 @@
+const _using_buffer = Ref(false)
+function enable_buffer()
+    _using_buffer[] = true
+    @printf "buffer: %s\n" using_buffer()
+    return _using_buffer[]
+end
+function disable_buffer()
+    _using_buffer[] = false
+    @printf "buffer: %s\n" using_buffer()
+    return _using_buffer[]
+end
+using_buffer() = _using_buffer[]
+##################################
+#
+#
 abstract type AbstractBuffer end
 struct heapcp end
 function with_alloc_buffer(f, buf::AbstractBuffer)
@@ -9,17 +24,26 @@ end
 function reset!(buf::AbstractBuffer)
 end
 
-function checkpoint_buffer(buf::AbstractBuffer)
-end
-
 function resize_buffer!(bsize, buf::AbstractBuffer)
 end
 
 function buffer_size(buf::AbstractBuffer)
 end
-function buffer_restore!(cp)
+
+function free_buffer!(buf::AbstractBuffer)
+end
+function allocate_buffer!(buf::AbstractBuffer)
+end
+function lanczo_cache_buffer(L, R, psi, buff::AbstractBuffer)
+    return L, R, psi
 end
 
+function lanczo_cache_buffer(L, R, H0, H1, psi, buff::AbstractBuffer)
+    return L, R, H0, H1, psi
+end
+####################################################
+#
+#
 struct HeapBuffer <: AbstractBuffer end
 
 with_alloc_buffer(f, ::HeapBuffer) = f()
@@ -28,17 +52,28 @@ reset!(::HeapBuffer) = nothing
 move_to_heap(A, ::HeapBuffer) = A
 checkpoint_buffer(::HeapBuffer) = heapcp()
 buffer_restore!(cp::heapcp) = nothing
-function DefaultBuffer(size=1048576)
-    ext = Base.get_extension(@__MODULE__, :iDMRGBUMPERExt)
-    if ext !== nothing
-        return ext.Allocator(size)
-    else
-        HeapBuffer()
-    end
-end
 
 function resize_buffer!(bsize, buf::HeapBuffer)
 end
 function buffer_size(buf::HeapBuffer)
     return UInt(0)
+end
+function free_buffer!(buf::HeapBuffer)
+end
+function allocate_buffer!(buf::HeapBuffer)
+end
+function lanczo_cache_buffer(L, R, psi, buff::HeapBuffer)
+    return L, R, psi
+end
+function lanczo_cache_buffer(L, R, H0, H1, psi, buff::HeapBuffer)
+    return L, R, H0, H1, psi
+end
+############################################################
+function DefaultBuffer(size=1048576)
+    ext = Base.get_extension(@__MODULE__, :iDMRGBUMPERExt)
+    if using_buffer()
+        return ext.Allocator(size)
+    else
+        HeapBuffer()
+    end
 end
